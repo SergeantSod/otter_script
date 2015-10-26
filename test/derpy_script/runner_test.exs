4a26f22b6220507a48eb5d171312a1baeb4c020b
Johannes Pirkl <johannespirkl@gmx.de>
2015-10-26 20:46:07 +0100

    More tests for the runner.

--- test/derpy_script/runner_test.exs
+++ test/derpy_script/runner_test.exs
@@ -16,6 +16,46 @@ defmodule DerpyScript.RunnerTest do
                                  """
   end
 
+  test "evaluates boolean literals" do
+    assert true == run """
+                       true
+                       """
+
+    assert false == run """
+                        false
+                        """
+  end
+
+  test "evaluates simple logical operations" do
+    assert true == run """
+                       not(false)
+                       """
+
+    assert false == run """
+                        not(true)
+                        """
+  end
+
+  test "evaluates if expressions without else clauses" do
+    assert 12 == run """
+                     if true then 12
+                     """
+
+    assert nil == run """
+                      if false then 12
+                      """
+  end
+
+  test "evaluates if expressions with else clauses" do
+    assert 12 == run """
+                     if true then 12 else 13
+                     """
+
+    assert 12 == run """
+                     if false then 13 else 12
+                     """
+  end
+
   test "evaluates assignments" do
     assert 12 == run """
                      a=12
@@ -67,9 +107,10 @@ defmodule DerpyScript.RunnerTest do
                      """
   end
 
-  test "evaluates higher-level functions" do
+  test "evaluates higher-order functions" do
     assert 12 == run """
                      adding:(offset)=>do
+                       # Look at my cool closure
                        (x)=>do
                          offset + x
                        end
@@ -81,5 +122,38 @@ defmodule DerpyScript.RunnerTest do
 
                      at_ten(adding(2))
                      """
+  end
+
+  test "evaluates higher-order functions with inline function literals as last argument" do
+    assert 12 == run """
+                     replicate:(times,start,f)=>do
+                       if times == 0 then do
+                         start
+                       end else do
+                         f(~(times - 1, start, f))
+                       end
+                     end
+
+                     #TODO This should have special syntax to prevent the horror that is javascript
+
+                     replicate(2,8,(x)=>do
+                       x + 2
+                     end)
+                     """
+  end
+
+  test "evaluates higher-order functions on built-in functions" do
+    assert "1\n2\n3\n" == capture_io fn ->
+      run """
+          loop:(from, to, fun)=>do
+            if from <= to then do
+              fun(from)
+              ~(from+1, to, fun)
+            end
+          end
+
+          loop(1, 3, print)
+          """
+    end
   end
 end
